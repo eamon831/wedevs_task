05f25c805ea891df7a3adfd80a537ecdbf819343
saiful <mdsaiful.rbs@gmail.com>
2024-05-31 22:06:39 +0600

    Pages Introduced and adding variables on my_app.dart

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '/app/routes/app_pages.dart';
+import '/flavors/build_config.dart';
+import '/flavors/env_config.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -9,10 +12,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final EnvConfig _envConfig = BuildConfig.instance.config;
+
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-
+    return GetMaterialApp(
+      title: _envConfig.appName,
+      initialRoute: AppPages.initial,
+      getPages: AppPages.routes,
     );
   }
 }
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,9 +1,15 @@
 import 'package:get/get.dart';
+import 'package:wedevs_task/app/pages/splash/views/splash_views.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
   static const initial = Routes.splash;
-  static final routes = [];
+  static final routes = [
+    GetPage(
+      name: _Paths.splash,
+      page:()=> SplashViews(),
+    ),
+  ];
 }
