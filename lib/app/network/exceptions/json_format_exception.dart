484e7d06cb1c2e6d0cb0db4b94507e3c85cb82b0
saiful <mdsaiful.rbs@gmail.com>
2024-05-31 22:23:42 +0600

    Network added on app

---
+++ lib/app/network/exceptions/json_format_exception.dart
@@ -0,0 +1,8 @@
+import '/app/network/exceptions/base_exception.dart';
+
+class JsonFormatException extends BaseException {
+  JsonFormatException(String message)
+      : super(
+          message: message,
+        );
+}
